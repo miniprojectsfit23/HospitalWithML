e4e2cb2d2c068a778db0fa88f0d8507464b1b0ab
Varun-Patkar <varunpatkar501@gmail.com>
2021-08-14 10:41:36 +0530

    Added updated navbar

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,4 @@ from . import views
 app_name="core"
 urlpatterns = [
 	path("",views.HomeView.as_view(),name="home"),
-	# path("register-doctor/",views.RegisterDoctorView.as_view(),name="register-doctor"),
-	# path("register-patient/",views.RegisterPatientView.as_view(),name="register-patient"),
-	# path("login-doctor/",views.LoginDoctorView.as_view(),name="login-doctor"),
-	# path("login-patient/",views.LoginPatientView.as_view(),name="login-patient"),
 ]
--- core/views.py
+++ core/views.py
@@ -11,3 +11,11 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = "Home"
         return context
+
+class LoginView(TemplateView):
+    template_name = "core/login.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Login"
+        return context
