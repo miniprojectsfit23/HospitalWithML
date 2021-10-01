be4dfbfea537038fda6f6a8d202fe6733361d254
Varun-Patkar <varunpatkar501@gmail.com>
2021-10-01 11:52:45 +0530

    Revert "Added pymongo instead of django as it is more reliable for connector"
    This reverts commit c3b6e5d189ae30915faf8e542a194199f96fa63e.

--- utils.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-import os
-MONGODB_USERNAME = os.environ.get("MONGODB_USERNAME")
-MONGODB_PASS = os.environ.get("MONGODB_PASS")
-client = pymongo.MongoClient('mongodb+srv://'+MONGODB_USERNAME+':'+MONGODB_PASS+'@cluster0.xz3j1.mongodb.net/HospitalWithMLDB?retryWrites=true&w=majority')
-db = client['HospitalWithMLDB']
