d5791e288562bafba607bed3784a082ac394a3ae
Varun-Patkar <varunpatkar501@gmail.com>
2021-10-06 15:48:43 +0530

    Update

--- HospitalWithML/settings.py
+++ HospitalWithML/settings.py
@@ -127,5 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL = 'core.User'
